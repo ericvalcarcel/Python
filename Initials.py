75841ba196dfd64430903cd9599bc1a6be0fc93d
evalcarcel12 <124469022+evalcarcel12@users.noreply.github.com>
2023-02-05 15:59:03 +0100

    Add files via upload

---
+++ Initials.py
@@ -0,0 +1,8 @@
+#My initials
+print ('EEEEE  V       V')
+print ('E      V       V')
+print ('E      V       V')
+print ('EEE    V       V')
+print ('E      V       V')
+print ('E        V   V')
+print ('EEEEE      V')
