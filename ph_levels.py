ed18aa50608120a28b9e3ce3b0efa843a21fdbb6
evalcarcel12 <124469022+evalcarcel12@users.noreply.github.com>
2023-02-05 15:59:37 +0100

    Add files via upload

---
+++ ph_levels.py
@@ -0,0 +1,8 @@
+ph = int(input('Whats the value of PH between 0 and 14?:  '))
+
+if ph  > 7:
+  print ('basic')
+elif ph < 7:
+    print('acidic')
+else:
+    print('neutral')
---
+++ temperature.py
@@ -0,0 +1,4 @@
+#Brooklyn ºF temp
+
+Cel = (24 - 32) / 1.8
+print (Cel)
