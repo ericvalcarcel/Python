3ac117ff74549dabecaed702a4abe2fb0899fdf8
evalcarcel12 <124469022+evalcarcel12@users.noreply.github.com>
2023-02-05 15:58:26 +0100

    Add files via upload

---
+++ Currency.py
@@ -0,0 +1,11 @@
+#Currency converter
+
+yuan = int(input('\nWhat do you have left in yuan? '))
+print (yuan)
+yen = int(input('\nWhat do you havel eft in yen? '))
+print (yen)
+won = int(input('\nWhat do you have left in won? '))
+print (won)
+print ('\nFinal amount:')
+print ((yen * 0.0076)+(yuan * 0.15)+(won * 0.00080)) 
+print ('USD')
