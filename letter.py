64538bea1f2d53aa9b4c7a4f13e0ece755694e6c
evalcarcel12 <124469022+evalcarcel12@users.noreply.github.com>
2023-02-05 15:59:12 +0100

    Add files via upload

---
+++ letter.py
@@ -0,0 +1,7 @@
+#Letter
+
+print ('04/02/2023')
+print ('Today i feel very motivated and proud of myself')
+print ('I will learn Python')
+print ('I hope you are proud of your younger version')
+print ('😾')
